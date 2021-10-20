76a71bb76fa4a563c0132112aed4bbf1cb69d5a9
Abel van Hoek <abelvanhoek@gmail.com>
2021-10-20 12:19:38 +0200

    add debounce func

--- src/general.js
+++ src/general.js
@@ -78,3 +78,19 @@ export function makeCns(...classNames) {
   classNames = _.flatten(classNames) // allow array args
   return classNames.filter(Boolean).join(" ")
 }
+
+export function debounce(func, wait, immediate) {
+  var timeout
+  return function () {
+    var context = this,
+      args = arguments
+    var later = function () {
+      timeout = null
+      if (!immediate) func.apply(context, args)
+    }
+    var callNow = immediate && !timeout
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+    if (callNow) func.apply(context, args)
+  }
+}
