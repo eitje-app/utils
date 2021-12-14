ce382d69b4255ad414cd06de82a04f7adb2b45d8
Abel van Hoek <abelvanhoek@gmail.com>
2021-12-14 14:10:15 +0100

    add regex util helpers

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ import * as number from './number'
 import * as object from './object'
 import * as functions from './functions'
 import * as array from './array'
+import * as regex from './regex'
 
-export default {...date, ...object, ...functions, ...string, ...collections, ...general, ...number, ...presentation, ...array}
+export default {...date, ...object, ...functions, ...string, ...collections, ...general, ...number, ...presentation, ...array, ...regex}
 
 // ...UI, ...string, ...collections, ...general, ...dev, ...number
 
---
+++ src/regex.js
@@ -0,0 +1,6 @@
+const emailCheck =
+  /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i
+
+export const isEmail = (val) => {
+  return emailCheck.test(val.trim())
+}
