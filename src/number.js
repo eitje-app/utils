68baf019a229dd0ff4d8551b767e0ec0e03bee2b
Abel van Hoek <abelvanhoek@gmail.com>
2022-07-28 23:26:30 +0200

    remove toggle from number file.. was duplicate

--- src/number.js
+++ src/number.js
@@ -2,61 +2,47 @@ import _ from 'lodash'
 import utils from './index'
 
 export function getMax(collection, key = 'updated_at') {
-  const items = collection.filter(a => a[key])
-  if(items.length === 0) return null
-  return items.reduce((a, b) => a[key] > b[key] ? a : b)
+  const items = collection.filter((a) => a[key])
+  if (items.length === 0) return null
+  return items.reduce((a, b) => (a[key] > b[key] ? a : b))
 }
 
 export function getMin(collection, key = 'updated_at') {
-  const items = collection.filter(a => a[key])
-  if(items.length === 0) return null
-  return items.reduce((a, b) => a[key] > b[key] ? b : a)
+  const items = collection.filter((a) => a[key])
+  if (items.length === 0) return null
+  return items.reduce((a, b) => (a[key] > b[key] ? b : a))
 }
 
 export function pct({total, active} = {}) {
-  return Math.round( (active / total) * 100)
+  return Math.round((active / total) * 100)
 }
 
+export function findAndReplace({oldItems = [], newItems = [], oldIden = 'id', newIden = 'id', mapFunc} = {}) {
+  if (newItems.length === 0) return oldItems
+  let items = oldItems.map((a) => Object.assign({}, a)) // deep-clone because we don't want to mess with life
 
-export function findAndReplace({oldItems = [], newItems = [], oldIden = "id", newIden = "id", mapFunc} = {}) {
-  
-  if(newItems.length === 0) return oldItems;
-  let items = oldItems.map(a => Object.assign({}, a)); // deep-clone because we don't want to mess with life
-  
-  newItems.forEach(newItem => {
-    const oldItem = items.find(i => i[oldIden] === newItem[newIden])
-    if(oldItem) {
+  newItems.forEach((newItem) => {
+    const oldItem = items.find((i) => i[oldIden] === newItem[newIden])
+    if (oldItem) {
       const idx = items.indexOf(oldItem)
       let combinedItem = {...oldItem, ...newItem}
-      if(mapFunc) {
+      if (mapFunc) {
         combinedItem = mapFunc({combined: combinedItem, old: oldItem, newItem})
       }
 
       items[idx] = combinedItem
-
     } else {
       items.push(newItem)
     }
   })
-  return items;
-
+  return items
 }
 
-export const mergeArrayOfObjects = (arr1, arr2, field = "id") => 
-  arr1.map(i => {
-    const other = arr2.find(i2 => i2[field] === i[field])
+export const mergeArrayOfObjects = (arr1, arr2, field = 'id') =>
+  arr1.map((i) => {
+    const other = arr2.find((i2) => i2[field] === i[field])
     return other ? {...i, ...other} : i
-  } )
-
-export function toggle(arr, nieuw) {
-  arr = _.cloneDeep(arr) 
-  if(arr.includes(nieuw)) {
-    arr = arr.filter(item => item !== nieuw)
-  } else {
-    arr.push(nieuw)
-  }
-  return arr;
-}
+  })
 
 export function money(int) {
   int = Number(int)
@@ -64,8 +50,7 @@ export function money(int) {
 }
 
 export function zeroOrNaN(num) {
-  return (num === 0 || isNaN(num))
+  return num === 0 || isNaN(num)
 }
 
-export const isOdd = num => num % 2 == 1;
-
+export const isOdd = (num) => num % 2 == 1
