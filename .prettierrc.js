8df16a691dffecca8f9f26b0c2c8245395899936
Abel <abel@eitje.app>
2024-10-28 17:39:29 +0100

    add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: false,
+  singleQuote: true,
+  trailingComma: "all",
+  semi: false,
+  printWidth: 140,
+  tabWidth: 2,
+  useTabs: true,
+  arrowParens: "avoid",
+};
